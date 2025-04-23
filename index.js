00d767edc95666111d8d7ef8677798be43e2ecbf
arunchauhan8991 <arunchauhan8991@gmail.com>
2025-04-23 06:55:20 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+import generateName   from "sillyname";
+
+var sillyName = generateName();
+
+console.log("My name is " + sillyName +".");
+
+import {randomSuperhero} from 'superheroes';
+
+var hero = randomSuperhero();
+
+console.log("My Superhero name is " + hero +".");
